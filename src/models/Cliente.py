6709ddccb248a7b0cf2e1c5b072ed27884888808
matias <matyaseze777@gmail.com>
2024-09-02 19:54:47 -0300

    Propuesta 1 - CRUD. Añadir, listar y borrar terminados. Falta el actualizar

--- src/models/Cliente.py
+++ src/models/Cliente.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class ClienteModel(db.Model):
+class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dni = db.Column(db.String(10), nullable=False, unique=True)
     nombre = db.Column(db.String(80), nullable=False)
@@ -10,13 +10,13 @@ class ClienteModel(db.Model):
     email = db.Column(db.String(120), nullable=False)
     telefono = db.Column(db.String(15), nullable=False)
 
-    def __init__(self, id, nombre, apellido, email, telefono, dni):
+    def __init__(self, id, dni, nombre, apellido, telefono, email):
         self.id = id
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
-        self.email = email
         self.telefono = telefono
+        self.email = email
 
     def __repr__(self):
         return f'<Cliente {self.nombre} {self.apellido}>'
@@ -30,8 +30,6 @@ class ClienteModel(db.Model):
             'email' : self.email,
             'telefono' : self.telefono,
         }
-
-
 
     @classmethod
     def get_clientes(self):
@@ -48,23 +46,39 @@ class ClienteModel(db.Model):
             raise Exception(ex)
         
     @classmethod
-    def añadir_cliente(self):
+    def añadir_cliente(self, cliente):
+        
         try:
-            db.session.add(self)
+            db.session.add(cliente)
             db.session.commit()
         except Exception as ex:
             raise Exception(ex)
+    @classmethod     
+    def editar_cliente(self,id, dni, nombre, apellido, telefono, email):
+        try:
+            if id:
+                self.id = id
+            if dni:
+                self.dni = dni
+            if nombre:
+                self.nombre = nombre
+            if apellido:
+                self.apellido = apellido
+            if email:
+                self.email = email
+            if telefono:
+                self.telefono = telefono
+            db.session.commit()
+        except Exception as ex:
+            db.session.rollback()
+            raise Exception(ex)
         
-
-    # @classmethod
-    # def actualizar_cliente(self, nombre=None, apellido=None, email=None, telefono=None):
-    #     try:
-    #         if nombre:
-    #             self.nombre = nombre
-    #         if apellido:
-    #             self.apellido = apellido
-    #         if email:
-    #             self.email = email
-    #         if telefono:
-    #             self.telefono = telefono
-    #     except:
+    # Eliminar cliente
+    @classmethod 
+    def eliminar_cliente(self, cliente):
+        try:
+            db.session.delete(cliente)
+            db.session.commit()
+        except Exception as ex:
+            db.session.rollback()
+            raise Exception(ex)
--- src/routes/Cliente.py
+++ src/routes/Cliente.py
@@ -1,36 +1,29 @@
 from flask import Blueprint, flash, render_template, request, redirect, url_for
-from models.Cliente import ClienteModel, db
+from models.Cliente import Cliente
 
 cliente_route = Blueprint('cliente_bp', __name__)
 
 @cliente_route.route('/alta', methods=['GET', 'POST'])
 def alta_cliente():
     if request.method == 'POST':
+        dni = request.form['dni']
         nombre = request.form['nombre']
         apellido = request.form['apellido']
-        email = request.form['email']
         telefono = request.form['telefono']
-        dni = request.form['dni']
+        email = request.form['email']
         
-        cliente_existente = ClienteModel.query.filter_by(dni=dni).first()
+        cliente_existente = Cliente.query.filter_by(dni=dni).first()
         if cliente_existente:
             flash("El cliente con este DNI ya existe.", "error")
             return redirect(url_for('cliente_bp.alta_cliente'))
-
-
-        nuevo_cliente = ClienteModel(nombre=nombre, apellido=apellido, email=email, telefono=telefono, dni=dni)
-        db.session.add(nuevo_cliente)
-        db.session.commit()
+        id = Cliente.query.order_by(Cliente.id.desc()).first()
+        print(f'Cliente con id: {id}')
+        nuevo_cliente = Cliente(id, dni, nombre, apellido, telefono, email)
+        Cliente.añadir_cliente(nuevo_cliente)
 
         flash("Cliente añadido exitosamente.", "success")
         return redirect(url_for('cliente_bp.listado_clientes'))
-        # except Exception as ex:
-        #     message = "Error al tratar de añadir un cliente"
-        #     cliente_existe = ClienteModel.query.filter_by(dni=dni).first()
-        #     if cliente_existe:
-        #         message = "Cliente ya existe, por favor ingrese otro"
-        #     flash(message, 'danger')
-                
+
     return render_template('alta_cliente.html')
 
 @cliente_route.route('/listado', methods=['GET'])
@@ -39,11 +32,56 @@ def listado_clientes():
     filtro_dni = request.args.get('dni')
 
     # Consultar la base de datos
-    clientes = ClienteModel.query.all()
+    clientes = Cliente.query.all()
 
     if filtro_nombre:
-        clientes = ClienteModel.query.filter(ClienteModel.nombre.ilike(f'%{filtro_nombre}%')).all()
+        clientes = Cliente.query.filter(Cliente.nombre.ilike(f'%{filtro_nombre}%')).all()
     if filtro_dni:
-        clientes = ClienteModel.query.filter_by(dni=filtro_dni).all()
+        clientes = Cliente.query.filter_by(dni=filtro_dni).all()
 
     return render_template('listado_clientes.html', clientes=clientes)
+
+@cliente_route.route('/editar/<int:id>', methods=['GET', 'POST'])
+
+
+
+def editar_cliente(id):
+
+    cliente = Cliente.query.get_or_404(id)
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['email']
+        telefono = request.form['telefono']
+
+        try:
+            Cliente.editar_cliente(nombre=nombre, apellido=apellido, email=email, telefono=telefono)
+            flash("Cliente actualizado exitosamente.", "success")
+            return redirect(url_for('cliente_bp.listado_clientes'))
+        except Exception as ex:
+            flash(f"Error al actualizar el cliente: {ex}", "error")
+
+    return render_template('editar_cliente.html', cliente=cliente)
+
+@cliente_route.route('/eliminar/<id>', methods=['GET','POST'])
+
+def eliminar_cliente(id):
+
+    if request.method == 'GET':
+        clientes = Cliente.query.all()
+        try:
+
+            cliente = Cliente.query.get(id)        
+            print(f'el cliente a eliminar es: {cliente}')
+            if cliente is not None:
+                Cliente.eliminar_cliente(cliente)
+                flash("Cliente eliminado exitosamente.", "success")
+                clientes = Cliente.query.all()
+                return render_template('listado_clientes.html', clientes=clientes)
+            else:
+                flash(f"Error al eliminar el cliente", "error")
+        except Exception as ex:
+                flash(f"Cliente no existe: {ex}", "error")
+                return render_template('listado_clientes.html', clientes=clientes)
+
